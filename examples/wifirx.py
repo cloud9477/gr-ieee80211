50de95292ab24288aef1275ba7aa016d27dabaa3
cloud9477 <cloud9477@gmail.com>
2022-07-30 13:38:35 -0400

    test mu-mimo

---
+++ examples/wifirx.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: Not titled yet
+# GNU Radio version: 3.9.5.0
+
+from distutils.version import StrictVersion
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print("Warning: failed to XInitThreads()")
+
+from gnuradio import analog
+from gnuradio import blocks
+import pmt
+from gnuradio import gr
+from gnuradio.filter import firdes
+from gnuradio.fft import window
+import sys
+import signal
+from PyQt5 import Qt
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+from gnuradio import network
+import ieee80211
+
+
+
+from gnuradio import qtgui
+
+class wifirx(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Not titled yet")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "wifirx")
+
+        try:
+            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+                self.restoreGeometry(self.settings.value("geometry").toByteArray())
+            else:
+                self.restoreGeometry(self.settings.value("geometry"))
+        except:
+            pass
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 20e6
+        self.freq = freq = 5420e6
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.network_udp_sink_0 = network.udp_sink(gr.sizeof_char, 1, '127.0.0.1', 9527, 0, 1400, False)
+        self.ieee80211_trigger_0 = ieee80211.trigger()
+        self.ieee80211_sync_0 = ieee80211.sync()
+        self.ieee80211_signal_0 = ieee80211.signal(1)
+        self.ieee80211_demod_0 = ieee80211.demod(1, 1, 2)
+        self.ieee80211_decode_0 = ieee80211.decode()
+        self.blocks_pdu_to_tagged_stream_0 = blocks.pdu_to_tagged_stream(blocks.byte_t, 'packet_len')
+        self.blocks_multiply_conjugate_cc_0 = blocks.multiply_conjugate_cc(1)
+        self.blocks_moving_average_xx_1 = blocks.moving_average_ff(64, 1, 4000, 1)
+        self.blocks_moving_average_xx_0 = blocks.moving_average_cc(48, 1, 4000, 1)
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_gr_complex*1, '/home/cloud/sdr/cmu-mupkt_2x2_1.bin', False, 0, 0)
+        self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
+        self.blocks_divide_xx_0 = blocks.divide_ff(1)
+        self.blocks_delay_0 = blocks.delay(gr.sizeof_gr_complex*1, 16)
+        self.blocks_complex_to_mag_squared_0 = blocks.complex_to_mag_squared(1)
+        self.blocks_complex_to_mag_0 = blocks.complex_to_mag(1)
+        self.analog_const_source_x_0 = analog.sig_source_c(0, analog.GR_CONST_WAVE, 0, 0, 0)
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.msg_connect((self.ieee80211_decode_0, 'out'), (self.blocks_pdu_to_tagged_stream_0, 'pdus'))
+        self.connect((self.analog_const_source_x_0, 0), (self.ieee80211_signal_0, 3))
+        self.connect((self.blocks_complex_to_mag_0, 0), (self.blocks_divide_xx_0, 0))
+        self.connect((self.blocks_complex_to_mag_squared_0, 0), (self.blocks_moving_average_xx_1, 0))
+        self.connect((self.blocks_delay_0, 0), (self.blocks_multiply_conjugate_cc_0, 0))
+        self.connect((self.blocks_divide_xx_0, 0), (self.ieee80211_trigger_0, 0))
+        self.connect((self.blocks_file_source_0, 0), (self.blocks_complex_to_mag_squared_0, 0))
+        self.connect((self.blocks_file_source_0, 0), (self.blocks_delay_0, 0))
+        self.connect((self.blocks_file_source_0, 0), (self.blocks_multiply_conjugate_cc_0, 1))
+        self.connect((self.blocks_file_source_0, 0), (self.ieee80211_signal_0, 2))
+        self.connect((self.blocks_file_source_0, 0), (self.ieee80211_sync_0, 1))
+        self.connect((self.blocks_moving_average_xx_0, 0), (self.blocks_complex_to_mag_0, 0))
+        self.connect((self.blocks_moving_average_xx_0, 0), (self.ieee80211_sync_0, 2))
+        self.connect((self.blocks_moving_average_xx_1, 0), (self.blocks_divide_xx_0, 1))
+        self.connect((self.blocks_multiply_conjugate_cc_0, 0), (self.blocks_moving_average_xx_0, 0))
+        self.connect((self.blocks_pdu_to_tagged_stream_0, 0), (self.network_udp_sink_0, 0))
+        self.connect((self.ieee80211_demod_0, 0), (self.ieee80211_decode_0, 0))
+        self.connect((self.ieee80211_signal_0, 0), (self.ieee80211_demod_0, 1))
+        self.connect((self.ieee80211_signal_0, 1), (self.ieee80211_demod_0, 2))
+        self.connect((self.ieee80211_sync_0, 0), (self.ieee80211_demod_0, 0))
+        self.connect((self.ieee80211_sync_0, 1), (self.ieee80211_signal_0, 1))
+        self.connect((self.ieee80211_sync_0, 0), (self.ieee80211_signal_0, 0))
+        self.connect((self.ieee80211_trigger_0, 0), (self.ieee80211_sync_0, 0))
+
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "wifirx")
+        self.settings.setValue("geometry", self.saveGeometry())
+        self.stop()
+        self.wait()
+
+        event.accept()
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+
+    def get_freq(self):
+        return self.freq
+
+    def set_freq(self, freq):
+        self.freq = freq
+
+
+
+
+def main(top_block_cls=wifirx, options=None):
+
+    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+
+    tb.start()
+
+    tb.show()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        Qt.QApplication.quit()
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
+
+    qapp.exec_()
+
+if __name__ == '__main__':
+    main()

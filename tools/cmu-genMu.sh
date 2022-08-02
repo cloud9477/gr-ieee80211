288234802d0456136cd3e7c8df9b77a1e4b269c1
cloud9477 <cloud9477@gmail.com>
2022-08-02 14:40:52 -0400

    mu-mimo demo

--- tools/cmu-genMu.sh
+++ tools/cmu-genMu.sh
@@ -4,11 +4,11 @@ echo "gen mu-mimo packet start"
 
 echo "get channel from station 0"
 
-pscp -r cloud@192.168.10.107:/home/cloud/sdr/cmu-chan0.bin /home/cloud/sdr/
+pscp -pw 7777 -r cloud@192.168.10.107:/home/cloud/sdr/cmu-chan0.bin /home/cloud/sdr/
 
 echo "get channel from station 1"
 
-pscp -r cloud@192.168.10.202:/home/cloud/sdr/cmu-chan1.bin /home/cloud/sdr/
+pscp -pw 7777 -r cloud@192.168.10.202:/home/cloud/sdr/cmu-chan1.bin /home/cloud/sdr/
 
 echo "run the python to generate mu-mimo signal"
 

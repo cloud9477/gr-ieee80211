5bd53fd656df555b6be82b9e765acdd39249a2c3
cloud9477 <cloud9477@gmail.com>
2023-04-29 19:17:40 -0400

    update siso perf

--- tools/performance/perf_siso.py
+++ tools/performance/perf_siso.py
@@ -70,58 +70,93 @@ def genMac80211UdpAmpduVht(udpPayloads):
         print("genMac80211UdpAmpduVht udpPakcets is not list type")
         return b""
 
-if __name__ == "__main__":
-    udpPayload200  = "123456789012345678901234567890abcdefghijklmnopqrst" * 4
-    perfPktNum = 10
-    perfSnrList = list(np.arange(0, 31, 1))
-    perfSigAmp = 0.18750000
-    perfSampNum = 0
-    perfRes = []
-    phy80211Ins = phy80211.phy80211(ifDebug=False)
-
-    ssMultiList = []
-    pkt = genMac80211UdpMPDU(udpPayload200)
-    for mcsIter in range(0, 8):
-        phy80211Ins.genFromMpdu(pkt, p8h.modulation(phyFormat=p8h.F.L, mcs=mcsIter, bw=p8h.BW.BW20, nSTS=1, shortGi=False))
-        ssFinal = phy80211Ins.genFinalSig(multiplier = 12.0, cfoHz = 0.0, num = perfPktNum, gap = True, gapLen = 1600)
-        ssMultiList.append(ssFinal)
-    # for mcsIter in range(0, 8):
-    #     phy80211Ins.genFromMpdu(pkt, p8h.modulation(phyFormat=p8h.F.HT, mcs=mcsIter, bw=p8h.BW.BW20, nSTS=1, shortGi=False))
-    #     ssFinal = phy80211Ins.genFinalSig(multiplier = 12.0, cfoHz = 0.0, num = perfPktNum, gap = True, gapLen = 1600)
-    #     ssMultiList.append(ssFinal)
-    # pkts = genMac80211UdpAmpduVht([udpPayload200])
-    # for mcsIter in range(0, 9):
-    #     phy80211Ins.genFromAmpdu(pkts, p8h.modulation(phyFormat=p8h.F.VHT, mcs=mcsIter, bw=p8h.BW.BW20, nSTS=1, shortGi=False), vhtPartialAid=0, vhtGroupId=0)
-    #     ssFinal = phy80211Ins.genFinalSig(multiplier = 12.0, cfoHz = 0.0, num = perfPktNum, gap = True, gapLen = 1600)
-    #     ssMultiList.append(ssFinal)
-    phy80211Ins.genMultiSigBinFile(ssMultiList, "/home/cloud/sdr/sig80211GenMultipleSiso", False)
-
-    
-    for snrIter in range(0, len(perfSnrList)):
-        tmpNoiseAmp = np.sqrt((perfSigAmp**2)/(10.0**(perfSnrList[snrIter]/10.0)))
+def testSnrPdrSiso(pktFormat, nMcs, listSnr, ampSig):
+    tmpPerfRes = []
+    for snrIter in range(0, len(listSnr)):
+        print("current snrIter %d of %d" % (snrIter, len(listSnr)))
+        tmpNoiseAmp = np.sqrt((ampSig**2)/(10.0**(listSnr[snrIter]/10.0)))
         os.system("python3 /home/cloud/sdr/gr-ieee80211/tools/performance/gr_siso.py " + str(tmpNoiseAmp) + " > /home/cloud/sdr/tmpSiso.txt &")
         tmpPreSize = 0
         tmpCurSize = 0
         while(True):
-            time.sleep(1)
+            time.sleep(0.5)
             tmpCurSize = os.path.getsize("/home/cloud/sdr/tmpSiso.txt")
-            print("cur size %d, pre size %d" % (tmpCurSize, tmpPreSize))
             if(tmpPreSize == tmpCurSize):
-                print("finish")
+                # print("finish, cur size %d, pre size %d" % (tmpCurSize, tmpPreSize))
                 break
             else:
                 tmpPreSize = tmpCurSize
-                print("continue")
+                # print("continue, cur size %d, pre size %d" % (tmpCurSize, tmpPreSize))
         os.system('pkill -f gr_siso.py')
 
-        resLine = open("/home/cloud/sdr/tmpSiso.txt").readlines()[-1]
-        resItems = resLine.split(",")
-        tmpRes = []
-        for i in range(3, 3+len(resItems)-6):
-            tmpRes.append(int(resItems[i].split(":")[1]))
-        perfRes.append(tmpRes)
-    
-    print(perfRes)
+        resFile = open("/home/cloud/sdr/tmpSiso.txt").readlines()
+        resFile.reverse()
+        resLine = ""
+        for each in resFile:
+            if("crc32" in each and pktFormat in each):
+                resLine = each
+                break
+        
+        if(len(resLine)):
+            resItems = resLine.split(",")
+            tmpRes = []
+            for i in range(3, 3+nMcs):
+                tmpRes.append(int(resItems[i].split(":")[1]))
+            tmpPerfRes.append(tmpRes)
+        else:
+            tmpPerfRes.append([0] * nMcs)
+    return tmpPerfRes
 
+if __name__ == "__main__":
+    udpPayload200  = "123456789012345678901234567890abcdefghijklmnopqrst" * 4
+    perfPktNum = 100
+    perfSnrList = list(np.arange(0, 31, 1))
+    perfSigAmp = 0.18750000
+    phy80211Ins = phy80211.phy80211(ifDebug=False)
 
+    pkt = genMac80211UdpMPDU(udpPayload200)
+    pkts = genMac80211UdpAmpduVht([udpPayload200])
+
+    ssMultiList = []
+    for mcsIter in range(0, 8):
+        phy80211Ins.genFromMpdu(pkt, p8h.modulation(phyFormat=p8h.F.L, mcs=mcsIter, bw=p8h.BW.BW20, nSTS=1, shortGi=False))
+        ssFinal = phy80211Ins.genFinalSig(multiplier = 12.0, cfoHz = 0.0, num = perfPktNum, gap = True, gapLen = 1600)
+        ssMultiList.append(ssFinal)
+    phy80211Ins.genMultiSigBinFile(ssMultiList, "/home/cloud/sdr/sig80211GenMultipleSiso", False)
+    legacyPerfRes = testSnrPdrSiso("legacy", 8, perfSnrList, perfSigAmp)
     
+    ssMultiList = []
+    for mcsIter in range(0, 8):
+        phy80211Ins.genFromMpdu(pkt, p8h.modulation(phyFormat=p8h.F.HT, mcs=mcsIter, bw=p8h.BW.BW20, nSTS=1, shortGi=False))
+        ssFinal = phy80211Ins.genFinalSig(multiplier = 12.0, cfoHz = 0.0, num = perfPktNum, gap = True, gapLen = 1600)
+        ssMultiList.append(ssFinal)
+    phy80211Ins.genMultiSigBinFile(ssMultiList, "/home/cloud/sdr/sig80211GenMultipleSiso", False)
+    htPerfRes = testSnrPdrSiso("ht", 8, perfSnrList, perfSigAmp)
+    
+    ssMultiList = []
+    for mcsIter in range(0, 9):
+        phy80211Ins.genFromAmpdu(pkts, p8h.modulation(phyFormat=p8h.F.VHT, mcs=mcsIter, bw=p8h.BW.BW20, nSTS=1, shortGi=False), vhtPartialAid=0, vhtGroupId=0)
+        ssFinal = phy80211Ins.genFinalSig(multiplier = 12.0, cfoHz = 0.0, num = perfPktNum, gap = True, gapLen = 1600)
+        ssMultiList.append(ssFinal)
+    phy80211Ins.genMultiSigBinFile(ssMultiList, "/home/cloud/sdr/sig80211GenMultipleSiso", False)
+    vhtPerfRes = testSnrPdrSiso("vht", 10, perfSnrList, perfSigAmp)
+
+    print(legacyPerfRes)
+    print(htPerfRes)
+    print(vhtPerfRes)
+
+    widths = [8]
+    heights = [4,4,4]
+    pltFig = plt.figure(figsize=(8,12))
+    spec = pltFig.add_gridspec(ncols=1, nrows=3, width_ratios=widths,
+                            height_ratios=heights, wspace=0.3, hspace=0.4)
+    ax = pltFig.add_subplot(spec[0, 0])
+    for i in range(0, 8):
+        ax.plot([each[i] for each in legacyPerfRes])
+    bx = pltFig.add_subplot(spec[1, 0])
+    for i in range(0, 8):
+        bx.plot([each[i] for each in htPerfRes])
+    cx = pltFig.add_subplot(spec[2, 0])
+    for i in range(0, 9):
+        cx.plot([each[i] for each in vhtPerfRes])
+    plt.show()
